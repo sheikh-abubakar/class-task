f942c75d9bd9344971ace5132075f40a5668f856
Muhammad Abubakar <163642750+sheikh-abubakar@users.noreply.github.com>
2024-10-12 13:38:29 +0500

    some chanages done

--- Practice-Problems/oop_hw2.cpp
+++ Practice-Problems/oop_hw2.cpp
@@ -1,55 +1,68 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-struct Book{
+int stringlen(const char* arr) {
+    int l = 0;
+    while (arr[l] != '\0') {
+        l++;
+    }
+    return l;
+}
+
+char* strcopy(const char* arr1) {
+    int l = stringlen(arr1);
+    char* arr2 = new char[l + 1];
+    for (int i = 0; i < l; i++) {
+        arr2[i] = arr1[i];
+    }
+    arr2[l] = '\0';
+    return arr2;
+}
+
+struct Book {
     char title[50];
     char author[50];
     int numOfCopies;
 };
-Book** initializeLibrary(int &numCategories, int* &numBooksinCategory, int*&freeIndexInCategory){
-    cout<<"enter number of catogeries"; 
-    cin>> numCategories;
-    Book** library = new Book*[numCategories];
-    numBooksinCatogery = new int[numCategories];
 
+Book** initializeLibrary(int& numCategories, int*& numBooksinCategory, int*& freeIndexInCategory) {
+    cout << "Enter number of categories: ";
+    cin >> numCategories;
+    Book** library = new Book*[numCategories];
+    numBooksinCategory = new int[numCategories];
     freeIndexInCategory = new int[numCategories];
 
-    for(int i =0; i< numCategories; i++){
-        cout<<"enter num of books : "<<i+1;
-        cin>>numBooksinCategory[i];
+    for (int i = 0; i < numCategories; i++) {
+        cout << "Enter number of books in category " << i + 1 << ": ";
+        cin >> numBooksinCategory[i];
         library[i] = new Book[numBooksinCategory[i]];
         freeIndexInCategory[i] = 0;
-
     }
     return library;
-
-
 }
 
-void addBook(Book** library, int numCategories, int* numBooksInCategory, int* freeIndexInCategory){
-    cout<<"enter cotagery number to add";
+void addBook(Book** library, int numCategories, int* numBooksInCategory, int* freeIndexInCategory) {
+    cout << "Enter category number to add: ";
     int cat_num;
-
-    cin>>cat_num;
-    if(freeIndexInCategory[cat_num-1] == numBooksInCategory){
-        cout<<"limit is full";
+    cin >> cat_num;
+    if (freeIndexInCategory[cat_num - 1] == numBooksInCategory[cat_num - 1]) {
+        cout << "Limit is full";
         return;
     }
-    cin>>library[cat_num-1][freeIndexInCategory[cat_num-1]].title;
-    cin>>library[cat_num-1][freeIndexInCategory[cat_num-1]].author;
-    cin>>library[cat_num-1][freeIndexInCategory[cat_num-1]].numOfCopies;
-    freeIndexInCategory[cat_num-1]++;
+    cin >> library[cat_num - 1][freeIndexInCategory[cat_num - 1]].title;
+    cin >> library[cat_num - 1][freeIndexInCategory[cat_num - 1]].author;
+    cin >> library[cat_num - 1][freeIndexInCategory[cat_num - 1]].numOfCopies;
+    freeIndexInCategory[cat_num - 1]++;
 }
 
-Book* searchBook(Book** library, int numCategories, int* numBooksInCategory, int* freeIndexInCategory){
+Book* searchBook(Book** library, int numCategories, int* numBooksInCategory, int* freeIndexInCategory) {
     char temp[50];
-    
+    cin.ignore(50, '\0');
     cin.getline(temp, 50);
-    for(int i =0; i<numBooksInCategory; i++){
-        for(int j =0; j< freeIndexInCategory[i];j++ ){
-            if(strcmp(library[i][j].title,temp)==0){
-                cout<<"'found";
+    for (int i = 0; i < numCategories; i++) {
+        for (int j = 0; j < freeIndexInCategory[i]; j++) {
+            if (strcmp(library[i][j].title, temp) == 0) {
+                cout << "Found" << endl;
                 return &library[i][j];
             }
         }
@@ -57,59 +70,69 @@ Book* searchBook(Book** library, int numCategories, int* numBooksInCategory, int
     return nullptr;
 }
 
-void updateCopies(Book** library, int numCategories, int* numBooksInCategory, int* freeIndexInCategory){
-   Book* ptr =  searchBook(library, numCategories, numBooksInCategory, freeIndexInCategory);
-    if(ptr == nullptr)
-    else{
-   cin>> ptr -> numOfCopies;
-   
-    
- }
+void updateCopies(Book** library, int numCategories, int* numBooksInCategory, int* freeIndexInCategory) {
+    Book* ptr = searchBook(library, numCategories, numBooksInCategory, freeIndexInCategory);
+    if (ptr == nullptr) {
+        cout << "Book not found" << endl;
+    } else {
+        cin >> ptr->numOfCopies;
+    }
 }
 
-printAllBooks(library, numCategories, numBooksInCategory, int* freeIndexInCategory);
-
-void releaseMemory(Book** library, int numCategories, int* numBooksInCategory, int* freeIndexInCategory)
-int main(){
-
-int numCategories = 0;
-int* numBooksInCategory = nullptr; //dynamic array
-int* freeIndexInCategory = nullptr; //dynamic array integer array
-
-//To be used while adding a new book to the library
-
-Book** library = initializeLibrary(numCategories, numBooksInCategory, freeIndexInCategory);
-int choice;
-while (choice != 5) {
-cout << "\nMenu:\n";
-cout << "1. Add Book\n";
-cout << "2. Search Book\n";
-cout << "3. Update Number of Copies\n";
-cout << "4. Print All Books\n";
-cout << "5. Exit\n";
-cout << "Enter your choice: ";
-cin >> choice;
-switch (choice) {
-        case 1:
-        addBook(library, numCategories, numBooksInCategory, freeIndexInCategory);
-        break;
-        case 2:
-        searchBook(library, numCategories, numBooksInCategory, freeIndexInCategory);
-        break;
-        case 3:
-        updateCopies(library, numCategories, numBooksInCategory, freeIndexInCategory);
-        break;
-        case 4:
-        printAllBooks(library, numCategories, numBooksInCategory, freeIndexInCategory);
-        break;
-        case 5:
-        cout << "Exiting the program..." << endl;
-        break;
-        default:
-        cout << "Invalid choice! Please try again." << endl;
+void printAllBooks(Book** library, int numCategories, int* numBooksInCategory, int* freeIndexInCategory) {
+    for (int i = 0; i < numCategories; i++) {
+        for (int j = 0; j < freeIndexInCategory[i]; j++) {
+            cout << library[i][j].title << " " << library[i][j].author << " " << library[i][j].numOfCopies << endl;
         }
+    }
 }
-    releaseMemory(library, numCategories, numBooksInCategory, freeIndexInCategory);
-return 0;
 
+void releaseMemory(Book** library, int numCategories, int* numBooksInCategory, int* freeIndexInCategory) {
+    for (int i = 0; i < numCategories; i++) {
+        delete[] library[i];
+    }
+    delete[] library;
+    delete[] numBooksInCategory;
+    delete[] freeIndexInCategory;
+    return;
+}
+
+int main() {
+    int numCategories = 0;
+    int* numBooksInCategory = nullptr;
+    int* freeIndexInCategory = nullptr;
+
+    Book** library = initializeLibrary(numCategories, numBooksInCategory, freeIndexInCategory);
+    int choice = 0;
+    while (choice != 5) {
+        cout << "\nMenu:\n";
+        cout << "1. Add Book\n";
+        cout << "2. Search Book\n";
+        cout << "3. Update Number of Copies\n";
+        cout << "4. Print All Books\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                addBook(library, numCategories, numBooksInCategory, freeIndexInCategory);
+                break;
+            case 2:
+                searchBook(library, numCategories, numBooksInCategory, freeIndexInCategory);
+                break;
+            case 3:
+                updateCopies(library, numCategories, numBooksInCategory, freeIndexInCategory);
+                break;
+            case 4:
+                printAllBooks(library, numCategories, numBooksInCategory, freeIndexInCategory);
+                break;
+            case 5:
+                cout << "Exiting the program..." << endl;
+                break;
+            default:
+                cout << "Invalid choice! Please try again." << endl;
+        }
+    }
+    releaseMemory(library, numCategories, numBooksInCategory, freeIndexInCategory);
+    return 0;
 }
